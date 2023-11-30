f096585f2592d215bfd04132ac9c0e899e5f7b57
PtacnikJan <jan.ptacnik@htlwy.at>
2023-11-30 09:06:32 +0100

    adminLayout

---
+++ vs (2)/WpfApp1/Window2.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaktionslogik für Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        public Window2()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                // Get the selected image file path
+                string imagePath = openFileDialog.FileName;
+
+                // Create a BitmapImage from the selected image file path
+                BitmapImage bitmapImage = new BitmapImage(new Uri(imagePath));
+
+                // Set the BitmapImage as the source of the Image control
+                image.Source = bitmapImage;
+            }
+        }
+    }
+}
