e0194d269dca6371d158df805760e88dc71c29a3
Agent_cake <agent.cake.hd@gmail.com>
2024-01-18 07:55:25 +0100

    added lampis serverconnect class

---
+++ MentoringLLC/Classes/ServerConnect.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SuperSimpleTcp;
+
+namespace MentoringLLC.Classes
+{
+    
+public class ServerConnect
+
+    {
+
+        public static void Connect()
+
+        {
+
+            SimpleTcpClient client = new SimpleTcpClient("45.142.114.49:9000");
+
+            // set events
+
+            client.Events.Connected += Connected;
+
+            client.Events.Disconnected += Disconnected;
+
+            client.Events.DataReceived += DataReceived;
+
+            // let's go!
+
+            client.Connect();
+
+            // once connected to the server...
+
+            while (true)
+
+            {
+
+                string consolePath = Console.ReadLine();
+
+                if (consolePath == "")
+
+                    consolePath = " ";
+
+                client.Send(consolePath);
+
+                Console.ReadKey();
+
+            }
+
+        }
+
+        static void Connected(object sender, ConnectionEventArgs e)
+
+        {
+
+            Console.WriteLine($"*** Server {e.IpPort} connected");
+
+        }
+
+        static void Disconnected(object sender, ConnectionEventArgs e)
+
+        {
+
+            Console.WriteLine($"*** Server {e.IpPort} disconnected");
+
+        }
+
+        static void DataReceived(object sender, DataReceivedEventArgs e)
+
+        {
+
+            Console.WriteLine($"{Encoding.UTF8.GetString(e.Data.Array, 0, e.Data.Count)}");
+
+        }
+
+    }
+}
