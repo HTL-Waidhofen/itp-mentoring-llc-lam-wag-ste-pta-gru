f1860a684289a1a2c101971bc0d3c6e7c2af5b62
Chriscraft57 <steiner.dichris@gmail.com>
2024-01-18 08:17:17 +0100

    Encode pfp in Edit_User page

--- MentoringLLC/Pages/Admin/Edit_User.xaml.cs
+++ MentoringLLC/Pages/Admin/Edit_User.xaml.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace MentoringLLC.Pages.Admin
 {
@@ -34,10 +36,19 @@ namespace MentoringLLC.Pages.Admin
                 openFileDialog.Filter = "Image files (*.png;*.jpeg)|*.png;*.jpeg|All files (*.*)|*.*";
                 if (openFileDialog.ShowDialog() == true)
                 {
+                    Byte[] bytes = File.ReadAllBytes(openFileDialog.FileName);
+                    string base64Encoded = Convert.ToBase64String(bytes);
+                    
+                    if(File.ReadAllBytes(openFileDialog.FileName).Length > 5242880) // 5MB = 5242880
+                    { 
+                        FeedbackMsg.Foreground = Brushes.Red;
+                        FeedbackMsg.Text = "Bild ist zu groß!";
+                    }
+                    else { 
                     ImagePreview.Source = new BitmapImage(new Uri(openFileDialog.FileName));
                     FeedbackMsg.Foreground = Brushes.Green;
                     FeedbackMsg.Text = "Bild erfolgreich hochgeladen";
-
+                    }
                 }
             }
             catch
