1e672ebb4d1f99816ef927a5546192c2d2cc951a
Agent_cake <agent.cake.hd@gmail.com>
2024-01-25 07:12:46 +0100

    added class Termin

--- MentoringLLC/Classes/Termin.cs
+++ MentoringLLC/Classes/Termin.cs
@@ -8,5 +8,26 @@ namespace MentoringLLC.Classes
 {
     internal class Termin
     {
+        string Titel { get; set; }
+        string Fach { get; set; }
+        int Teilnehmermoeglich { get; set; }
+        int Teilnehmeranzahl { get; set; }
+        DateTime Datum { get; set; }
+        string Ort { get; set; }
+        string Beschreibung { get; set; }
+
+        public Termin() { }
+
+        public Termin(string titel, string fach, int teilnehmeranzahl, DateTime datum,  string ort, string beschreibung="") 
+        { 
+            Titel = titel;
+            Fach = fach;
+            Teilnehmermoeglich= teilnehmeranzahl;
+            Datum = datum;
+            Ort = ort;
+            Beschreibung = beschreibung;
+           
+        }
+
     }
 }
