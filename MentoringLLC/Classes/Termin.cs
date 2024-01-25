37d6e62620a6b2242da6b0b10279fdbf6037cd8b
Agent_cake <agent.cake.hd@gmail.com>
2024-01-25 07:03:33 +0100

    added class termin

---
+++ MentoringLLC/Classes/Termin.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MentoringLLC.Classes
+{
+    internal class Termin
+    {
+    }
+}
